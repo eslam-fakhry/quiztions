ab51ccd06cd1bd16ee7f94e5f2fb3d93eafba7c0
islam fakhry <islam1000man@gmail.com>
2019-07-07 05:49:50 +0200

    initial commit

---
+++ src/components/Question.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div class="question">what is the longest river in africa</div>
+        <div class="options">
+            <v-btn v-for="(option,index) in options"
+                   :key="option"
+                   @click.prevent="answer(index)"
+            >{{option}}</v-btn>
+        </div>
+        <div class="feedback" v-if="feedback"> {{feedback}}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Question",
+        data() {
+            return {
+                options: ['Nile','Rhine','amazon'],
+                feedback:'',
+            }
+        },
+        methods: {
+            validate(index) {
+                return index ===1
+            },
+            answer(index){
+
+                this.feedback = this.validate(index)?'you are right': 'you are wrong haha'
+            },
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>

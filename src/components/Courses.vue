85bead1f04e2c1a912e31d8aa72b5670ad0b590e
Eslam Fakhry <islam1000man@gmail.com>
2019-08-16 06:59:02 +0200

    load courses in created

--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -1,45 +1,19 @@
 <template>
     <v-container>
         <h1 class="text-center">Courses</h1>
-        <LogoutBtn>
-            <v-btn>logout</v-btn>
-        </LogoutBtn>
-        <v-btn @click="showToast">show_toast</v-btn>
         <v-btn @click="$store.dispatch('addCoursesToUser')">addCoursesToUser</v-btn>
-        <v-btn @click="fetchUserCourses">fetchUserCourses</v-btn>
 
         <v-layout wrap>
 
-            <v-flex xs12 sm6 md4 lg3 v-for="(course, id) in courses" :key="`course--${id}`">
+            <v-flex xs12 sm6 md4 v-for="(course, id) in courses" :key="`course--${id}`">
                 <v-card
                         :elevation="0"
                         outlined
                         flat
-                        class="mx-4 my-4"
+                        class="mx-4 my-4 pt-6 pb-10 px-4"
+                        :to="{name:'course',params:{course_id:id}}"
                 >
-                    <v-img
-                            class="white--text"
-                            height="200px"
-                            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-                    >
-                        <v-container fill-height fluid>
-                            <v-layout fill-height>
-                                <v-flex xs12 align-end flexbox>
-                                    <span class="headline">{{course.name}}</span>
-                                </v-flex>
-                            </v-layout>
-                        </v-container>
-                    </v-img>
-                    <v-card-title>
-                        <div>
-                            <span class="grey--text">10 lessons</span><br>
-                            <span>How to easily understand such a great topic</span><br>
-                            <span class="font-italic font-weight-bold">Dr. John harper</span>
-                        </div>
-                    </v-card-title>
-                    <v-card-actions>
-                        <v-btn text color="orange" :to="{name:'course',params:{course_id:id}}">Enter</v-btn>
-                    </v-card-actions>
+                    <span class="headline">{{course.name}}</span>
                 </v-card>
 
             </v-flex>
@@ -48,27 +22,24 @@
 </template>
 
 <script>
-    import {createNamespacedHelpers } from 'vuex'
+    import {createNamespacedHelpers} from 'vuex'
+
     const {mapState, mapActions} = createNamespacedHelpers('user')
 
-    import LogoutBtn from './auth/LogoutBtn'
 
     export default {
         name: "Courses",
-        components: {LogoutBtn},
         computed: {
             // todo add loading state
             ...mapState(['courses']),
         },
-        methods:{
+        methods: {
             ...mapActions(['fetchUserCourses']),
-            showToast(){
-                this.$store.commit('snackbars/SET_TOAST', {message: "you're successfully logged in",})
-            },
+
         },
 
-        created(){
-
+        created() {
+            this.fetchUserCourses()
         },
     }
 </script>

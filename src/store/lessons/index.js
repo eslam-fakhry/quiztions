6f02cfda7bdfa5ccbc7c68e99edaeade187255f7
Eslam Fakhry <islam1000man@gmail.com>
2019-08-18 16:17:54 +0200

    removed unnecessary getters in lessons module

--- src/store/lessons/index.js
+++ src/store/lessons/index.js
@@ -14,8 +14,8 @@ export default {
     },
     actions: {
 
-        async fetchLesson({getters, commit}, {id}) {
-            let lesson = getters.getLesson(id);
+        async fetchLesson({getters, state, commit}, {id}) {
+            let lesson = state.lessons[id];
             if (lesson) return lesson
             // fetch from server
             return fb.refs.lessonsRef
@@ -29,14 +29,14 @@ export default {
             // otherwise show 404 page
             // todo:show 404 page
         },
-        async createLesson({commit, state,rootState}, {name,courseId}) {
+        async createLesson({commit, state, rootState}, {name, courseId}) {
             // Get a key for a new Lesson.
             const newLessonKey = fb.db.ref().child('lessons').push().key;
             // Write the new lesson's data simultaneously in the lessons list and the user's lesson list.
             const updates = {};
             updates['/lessons/' + newLessonKey] = {
                 name,
-                teacherId:rootState.user.uid,
+                teacherId: rootState.user.uid,
             };
             updates['/courses/' + courseId + '/lessons/' + newLessonKey] = {
                 name,
@@ -44,6 +44,7 @@ export default {
             await fb.db.ref().update(updates);
             return newLessonKey
         },
+
 
         // async deleteLesson({commit, state, rootState}, {id}) {
         //
@@ -58,15 +59,5 @@ export default {
         //     await fb.db.ref().update(updates);
         // },
 
-    },
-    getters: {
-        lessons(state) {
-            return state.lessons
-        },
-        getLesson(state) {
-            return (id) => {
-                return state.lessons[id]
-            }
-        },
     },
 }

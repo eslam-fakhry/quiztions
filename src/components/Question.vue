82678b0dec1401498fc0670a850a63c0b23ed541
Eslam Fakhry <eslamPhakhry@gmail.com>
2019-08-27 17:08:24 +0200

    Refactor Question component's layout

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -11,46 +11,59 @@
                 style="height: 100%"
         >
 
-            <v-flex shrink v-if="isBodyString">
-                <div class="text-center pb-6 mt-6">{{question.body}}</div>
-            </v-flex>
-            <v-flex shrink v-else>
+            <v-flex
+                    shrink
+            >
                 <div
+                        v-if="isBodyString"
+                        class="text-center pb-6 mt-6"
+                >
+                    {{question.body}}
+                </div>
+                <div
+                        v-else
                         v-for="(bodyItem,i) in  question.body"
                         class="text-center pb-6 mt-6"
                         :key="`bodyItem--${i}`"
                 >
-                    <span v-if="bodyItem.text" v-text="bodyItem.text"></span>
+                    <span
+                            v-if="bodyItem.text"
+                            v-text="bodyItem.text"
+                   />
                 </div>
             </v-flex>
 
-            <v-flex>
-                <v-layout column style="height: 100%">
-                    <v-flex shrink>
-                        <component
-                                :is="answerComponentName"
-                                ref="Answer"
-                                @answered="answered = true"
-                                :key="question.id"
-                                :ui-state="uiState"
-                        ></component>
-
-                    </v-flex>
-                    <v-flex grow class="d-flex justify-center align-center">
-                       <QuestionFeedback :ui-state="uiState" :feedback="feedback"/>
-                    </v-flex>
-                    <v-flex shrink class="text-center pb-4">
-                        <v-btn
-                                outlined
-                                @click="answer"
-                                :disabled="uiState.NOT_ANSWERED"
-                                :loading="validatingAnswer"
-                                v-text="submitBtnText"
-                        />
-                    </v-flex>
-                </v-layout>
+            <v-flex
+                    shrink
+            >
+                <component
+                        :is="answerComponentName"
+                        ref="Answer"
+                        @answered="answered = true"
+                        :key="question.id"
+                        :ui-state="uiState"
+                ></component>
             </v-flex>
 
+            <v-flex
+                    grow
+                    class="d-flex justify-center align-center"
+            >
+                <QuestionFeedback :ui-state="uiState" :feedback="feedback"/>
+            </v-flex>
+
+            <v-flex
+                    shrink
+                    class="text-center pb-4"
+            >
+                <v-btn
+                        outlined
+                        @click="answer"
+                        :disabled="uiState.NOT_ANSWERED"
+                        :loading="validatingAnswer"
+                        v-text="submitBtnText"
+                />
+            </v-flex>
         </v-layout>
     </div>
 </template>
@@ -88,7 +101,7 @@
             InputAnswer,
         },
 
-        mixins:[QuestionMixin,],
+        mixins: [QuestionMixin,],
 
         props: {
             questionId: {type: String}

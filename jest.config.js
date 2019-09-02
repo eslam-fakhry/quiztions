2d6264d5f98356f1e209b8de1bc12f121b8edbbb
Eslam Fakhry <eslamPhakhry@gmail.com>
2019-09-02 07:32:34 +0200

    Update transformIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
     '^.+\\.jsx?$': 'babel-jest'
   },
   transformIgnorePatterns: [
-    '/node_modules/'
+    // '<rootDir>/node_modules/',
   ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'

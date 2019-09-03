5a7df2aac6ef77939dbdef23b863527b028bfe97
Eslam Fakhry <eslamPhakhry@gmail.com>
2019-09-03 23:31:48 +0200

    Add animated components for succeeded and failed lessons

--- src/components/ResultMessage.vue
+++ src/components/ResultMessage.vue
@@ -3,18 +3,22 @@
         <h6>{{message || (result ==='success'?'you succeeded':'you failed')}}</h6>
         <div v-if="result ==='success'">
             <router-link :to="{name:'home'}">Go to home page</router-link>
+            <SuccessBadge/>
         </div>
         <div v-else>
-
+            <router-link :to="{name:'home'}">Go to home page</router-link>
+            <FailedEmoji/>
         </div>
 
     </div>
 </template>
 
 <script>
+    import SuccessBadge from "@/components/SuccessBadge";
+    import FailedEmoji from "@/components/Failed-emoji";
     export default {
         name: "ResultMessage",
-
+        components: {FailedEmoji, SuccessBadge},
         props: {
             message: {type: String},
             result: { type: String, required: true },

f4587e1cc6c31b132983d04a423135e1186962d9
Eslam Fakhry <eslamPhakhry@gmail.com>
2019-09-04 14:23:45 +0200

    Remove drawer

--- src/layouts/TheToolbar.vue
+++ src/layouts/TheToolbar.vue
@@ -1,86 +1,5 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
     <div>
-        <v-navigation-drawer
-                v-if="false"
-                v-model="drawer"
-                :clipped="$vuetify.breakpoint.lgAndUp"
-                app
-        >
-            <v-list dense>
-                <template v-for="item in items">
-                    <v-layout
-                            v-if="item.heading"
-                            :key="item.heading"
-                            align-center
-                    >
-                        <v-flex xs6>
-                            <v-subheader
-                                    v-if="item.heading"
-                                    v-text="item.heading"
-                            />
-                        </v-flex>
-                        <v-flex
-                                xs6
-                                class="text-center"
-                        >
-                            <a
-                                    href="#!"
-                                    class="body-2 black--text"
-                            >
-                                EDIT
-                            </a>
-                        </v-flex>
-                    </v-layout>
-                    <v-list-group
-                            v-else-if="item.children"
-                            :key="item.text"
-                            v-model="item.model"
-                            :prepend-icon="item.model ? item.icon : item['icon-alt']"
-                            append-icon=""
-                    >
-                        <template v-slot:activator>
-                            <v-list-item>
-                                <v-list-item-content>
-                                    <v-list-item-title
-                                            v-text="item.text"
-                                    />
-                                </v-list-item-content>
-                            </v-list-item>
-                        </template>
-                        <v-list-item
-                                v-for="(child, i) in item.children"
-                                :key="i"
-                                @click=""
-                        >
-                            <v-list-item-action v-if="child.icon">
-                                <v-icon>{{ child.icon }}</v-icon>
-                            </v-list-item-action>
-                            <v-list-item-content>
-                                <v-list-item-title
-                                        v-text="child.text"
-                                />
-                            </v-list-item-content>
-                        </v-list-item>
-                    </v-list-group>
-                    <v-list-item
-                            v-else
-                            :key="item.text"
-                            @click=""
-                    >
-                        <v-list-item-action>
-                            <v-icon
-                                    v-text="item.icon"
-                            />
-                        </v-list-item-action>
-                        <v-list-item-content>
-                            <v-list-item-title
-                           v-text="item.text"
-                            />
-                        </v-list-item-content>
-                    </v-list-item>
-                </template>
-            </v-list>
-        </v-navigation-drawer>
         <v-app-bar
                 :clipped-left="$vuetify.breakpoint.lgAndUp"
                 app
@@ -144,25 +63,6 @@
         name: "TheToolbar",
 
         components: {UserPicture, LogoutBtn},
-
-        data: () => ({
-            dialog: false,
-            drawer: null,
-            items: [
-                {icon: 'home', text: 'Home'},
-                {icon: 'book', text: 'Browse Courses'},
-                // {icon: 'content_copy', text: 'Duplicates'},
-                // {
-                //     icon: 'keyboard_arrow_up',
-                //     'icon-alt': 'keyboard_arrow_down',
-                //     text: 'Labels',
-                //     model: true,
-                //     children: [
-                //         {icon: 'add', text: 'Create label'},
-                //     ],
-                // },
-            ],
-        }),
     }
 </script>
 

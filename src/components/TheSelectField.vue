329922a47a04195f05a0aaae5cf8cd80ab7b8ec5
Eslam Fakhry <eslamPhakhry@gmail.com>
2019-09-04 12:51:08 +0200

    Create default TextField and SelectField components

---
+++ src/components/TheSelectField.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-select
+            :items="items"
+            :label="label"
+            :value="value"
+            @input="$emit('input',$event)"
+            hide-details
+            outlined
+    />
+</template>
+
+<script>
+    export default {
+        name: "TheSelectField",
+
+        props:{
+            items:{},
+            label:{},
+            value:{},
+        }
+    }
+</script>
+
---
+++ src/components/TheTextField.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-text-field
+            :value="value"
+            @input="$emit('input',$event)"
+            @change="$emit('change',$event)"
+            :label="label"
+            :placeholder="placeholder"
+            hide-details
+            outlined
+    />
+</template>
+
+<script>
+    export default {
+        name: "TheTextField",
+
+        props: {
+            value: {},
+            label: {},
+            placeholder: {}
+        }
+    }
+</script>
+
+

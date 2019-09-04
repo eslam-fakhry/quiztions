e7d62acee50457bd08e23553256a16b1a6fb5a19
Eslam Fakhry <eslamPhakhry@gmail.com>
2019-09-04 20:23:24 +0200

    Refactor firebase-facade

--- src/services/firebase-facade/index.js
+++ src/services/firebase-facade/index.js
@@ -4,17 +4,25 @@ import {showSnackbar} from "@/utils";
 
 const allowableRefs = ['questions', 'lessons', 'courses', 'teachers', 'students', 'rightAnswers']
 
-// todo move this ta an appropriate place
 function fetchResource(resourceName, id) {
     if (!allowableRefs.includes(resourceName)) throw new Error('Reference name is not allowed')
-
-    return fb.db.ref(resourceName)
-        .child(id)
+    return fb.db.ref(`${resourceName}/${id}`)
         .once('value')
         .then(snap => {
             if (!snap.val()) return Promise.reject('no such thing')
             return snap.val()
         })
+}
+
+async function fetchSyncedResource(resourceName, id, cb) {
+    return await new Promise((resolve, reject) => {
+        if (!allowableRefs.includes(resourceName)) reject('Reference name is not allowed')
+        fb.db.ref(`${resourceName}/${id}`)
+            .on('value', (snap) => {
+                cb(snap)
+                resolve()
+            })
+    })
 }
 
 function fetchCourses() {
@@ -26,32 +34,18 @@ function fetchCourses() {
         })
 }
 
-async function fetchSyncedResource(resourceName, id, cb) {
-    return await new Promise((resolve, reject) => {
-        if (!allowableRefs.includes(resourceName)) reject('Reference name is not allowed')
-        fb.db.ref(resourceName)
-            .child(id)
-            .on('value', (snap) => {
-                cb(snap)
-                resolve()
-            })
-    })
-}
-
 async function fetchUserCourses(job, cb) {
     return await new Promise((resolve, reject) => {
         if (!['student', 'teacher'].includes(job)) reject('Reference name is not allowed')
         fb.db.ref(job + 's')
             .child(fb.auth.currentUser.uid)
             .child('courses')
-
             .on('value', (snap) => {
                 cb(snap)
                 resolve()
             })
     })
 }
-
 
 async function createQuestion({question, rightAnswer, lessonId}) {
     const questionData = {
@@ -60,47 +54,51 @@ async function createQuestion({question, rightAnswer, lessonId}) {
         teacherId: fb.auth.currentUser.uid,
     }
     const newQuestionKey = fb.db.ref('questions').push().key
-    const updates = {}
-    updates['/questions/' + newQuestionKey] = questionData
-    updates['/lessons/' + lessonId + '/questions/' + newQuestionKey] = newQuestionKey
-    updates['/rightAnswers/' + newQuestionKey] = rightAnswer
+    const updates = buildNewQuestionUpdates(newQuestionKey, questionData, lessonId, rightAnswer);
     await fb.db.ref().update(updates)
     return newQuestionKey
 }
 
-async function createLesson({name, courseId}) {
+function buildNewQuestionUpdates(questionId, questionData, lessonId, rightAnswer) {
+    const updates = {}
+    updates[`/questions/${questionId}`] = questionData
+    updates[`/lessons/${lessonId}/questions/${questionId}`] = questionId
+    updates[`/rightAnswers/${questionId}`] = rightAnswer
+    return updates;
+}
 
+async function createLesson({name, courseId}) {
     const lessonData = {
         name,
         courseId,
         teacherId: fb.auth.currentUser.uid,
     }
     const newLessonKey = fb.db.ref('lessons').push().key
-    const updates = {}
-    updates['/lessons/' + newLessonKey] = lessonData
-    updates['/courses/' + courseId + '/lessons/' + newLessonKey] = {
-        name,
-    }
+    const updates = buildNewLessonUpdates(newLessonKey, lessonData, courseId, name);
     await fb.db.ref().update(updates)
-    return {
-        key: newLessonKey,
-        lesson: lessonData,
-    }
+    return {key: newLessonKey, lesson: lessonData,}
+}
+
+function buildNewLessonUpdates(lessonId, lessonData, courseId, name) {
+    const updates = {}
+    updates[`/lessons/${lessonId}`] = lessonData
+    updates[`/courses/${courseId}/lessons/${lessonId}`] = {name,}
+    return updates;
 }
 
 async function createCourse({name}) {
     const newCourseKey = fb.db.ref('courses').push().key
-
-    const updates = {}
-    updates['/courses/' + newCourseKey] = {
-        name,
-        teacherId: fb.auth.currentUser.uid,
-    }
-    updates['/teachers/' + fb.auth.currentUser.uid + '/courses/' + newCourseKey] = {
-        name,
-    }
+    const updates = buildNewCourseUpdates(newCourseKey, name);
     await fb.db.ref().update(updates)
     return newCourseKey
+}
+
+function buildNewCourseUpdates(courseId, name) {
+    const updates = {}
+    const teacherId = fb.auth.currentUser.uid;
+    updates[`/courses/${courseId}`] = {name, teacherId,}
+    updates[`/teachers/${teacherId}/courses/${courseId}`] = {name,}
+    return updates;
 }
 
 function enrollInCourse({id: courseId, name}) {
@@ -113,65 +111,47 @@ function leaveCourse({id: courseId}) {
 
 async function toggleEnrollmentInCourse({courseId, name}, toEnroll = true) {
     const userId = fb.auth.currentUser.uid
-    const updates = {}
-    if (toEnroll) {
-        updates[`/courseStudents/${courseId}/${userId}`] = true
-        updates[`students/${userId}/courses/${courseId}`] = {name}
-    } else {
-        updates[`/courseStudents/${courseId}/${userId}`] = null
-        updates[`students/${userId}/courses/${courseId}`] = null
-    }
+    const updates = buildCourseEnrollmentUpdates(toEnroll, courseId, userId, name);
     await fb.db.ref().update(updates)
 }
 
-async function deleteQuestion(questionId, lessonId) {
-    let updates = {};
-    updates['/lessons/' + lessonId + '/questions/' + questionId] = null;
-    updates = Object.assign(updates, questionDeletionDependencies(questionId))
-
-    try {
-        await fb.db.ref().update(updates);
-    } catch (e) {
-        if (e.code === "PERMISSION_DENIED") {
-            showSnackbar('You have no authentication to complete this process', 'error')
-            return
-        }
-        showSnackbar('Something went wrong', 'error')
-    }
-}
-
-async function deleteLesson(lessonId) {
-    const value = await fetchResource('lessons', lessonId)
-    const questions = value.questions ? Object.keys(value.questions) : []
-    const courseId = value.courseId
-
-    let updates = {};
-    updates['/courses/' + courseId + '/lessons/' + lessonId] = null;
-    updates = Object.assign(updates, lessonDeletionDependencies(lessonId, questions))
-
-    fb.db.ref('lessons/' + lessonId).off('value')
-
-    try {
-        await fb.db.ref().update(updates);
-    } catch (e) {
-        if (e.code === "PERMISSION_DENIED") {
-            showSnackbar('You have no authentication to complete this process', 'error')
-            return
-        }
-        showSnackbar('Something went wrong', 'error')
-    }
+function buildCourseEnrollmentUpdates(toEnroll, courseId, userId, name) {
+    const updates = {}
+    updates[`/courseStudents/${courseId}/${userId}`] = toEnroll ? true : null
+    updates[`students/${userId}/courses/${courseId}`] = toEnroll ? {name} : null
+    return updates;
 }
 
 async function deleteCourse(courseId) {
     const course = await fetchResource('courses', courseId)
     const lessons = course.lessons ? Object.keys(course.lessons) : []
+    let updates = await buildCourseDeletionUpdates(courseId, lessons);
+    try {
+        await fb.db.ref().update(updates);
+    } catch (e) {
+        if (e.code === "PERMISSION_DENIED") {
+            showSnackbar('You have no authentication to complete this process', 'error')
+            return
+        }
+        showSnackbar('Something went wrong', 'error')
+    }
+}
 
+async function buildCourseDeletionUpdates(courseId, lessons) {
     let updates = {};
+    const teacherId = fb.auth.currentUser.uid
     updates = Object.assign(updates, await courseDeletionDependencies(courseId, lessons))
     updates = Object.assign(updates, await courseEnrolledDeletionDependencies(courseId))
-    updates['/teachers/' + fb.auth.currentUser.uid + '/courses/' + courseId] = null;
+    updates[`/teachers/${teacherId}/courses/${courseId}`] = null;
+    return updates;
+}
 
-    fb.db.ref('courses/' + courseId).off('value')
+async function deleteLesson(lessonId) {
+    const value = await fetchResource('lessons', lessonId)
+    const questions = value.questions ? Object.keys(value.questions) : []
+    const {courseId} = value
+
+    let updates = buildLessonDeletionUpdates(lessonId, courseId, questions);
 
     try {
         await fb.db.ref().update(updates);
@@ -184,32 +164,39 @@ async function deleteCourse(courseId) {
     }
 }
 
-/*
-* Deletion dependencies
-* For multi-path update
-*/
-function questionDeletionDependencies(id) {
-    const updates = {}
-    updates['/questions/' + id] = null
-    updates['/rightAnswers/' + id] = null
-    return updates
+function buildLessonDeletionUpdates(lessonId, courseId, questions) {
+    let updates = {};
+    updates[`/courses/${courseId}/lessons/${lessonId}`] = null;
+    updates = Object.assign(updates, lessonDeletionDependencies(lessonId, questions))
+    return updates;
 }
 
-function lessonDeletionDependencies(lessonId, questions) {
-    fb.db.ref('lessons/' + lessonId).off('value')
-    let updates = {};
-    updates['/lessons/' + lessonId] = null;
-    questions.forEach((questionId) => {
-        updates = Object.assign(updates, questionDeletionDependencies(questionId))
-    })
-    return updates
+async function deleteQuestion(questionId, lessonId) {
+    let updates = buildQuestionDeletionUpdates(lessonId, questionId);
+    try {
+        await fb.db.ref().update(updates);
+    } catch (e) {
+        if (e.code === "PERMISSION_DENIED") {
+            showSnackbar('You have no authentication to complete this process', 'error')
+            return
+        }
+        showSnackbar('Something went wrong', 'error')
+    }
 }
+
+function buildQuestionDeletionUpdates(lessonId, questionId) {
+    let updates = {};
+    updates[`/lessons/${lessonId}/questions/${questionId}`] = null;
+    updates = Object.assign(updates, questionDeletionDependencies(questionId))
+    return updates;
+}
+
+// Deletion dependencies for multi-path update
 
 async function courseDeletionDependencies(courseId, lessons) {
     fb.db.ref('courses/' + courseId).off('value')
     let updates = {};
-    updates['/courses/' + courseId] = null;
-
+    updates[`/courses/${courseId}`] = null;
     await Promise.all(lessons.map(async (lessonId) => {
         const lesson = await fetchResource('lessons', lessonId)
         const questions = lesson.questions ? Object.keys(lesson.questions) : []
@@ -218,14 +205,30 @@ async function courseDeletionDependencies(courseId, lessons) {
     return updates
 }
 
+function lessonDeletionDependencies(lessonId, questions) {
+    fb.db.ref('lessons/' + lessonId).off('value')
+    let updates = {};
+    updates[`/lessons/${lessonId}`] = null;
+    questions.forEach((questionId) => {
+        updates = Object.assign(updates, questionDeletionDependencies(questionId))
+    })
+    return updates
+}
+
+function questionDeletionDependencies(id) {
+    const updates = {}
+    updates['/questions/' + id] = null
+    updates['/rightAnswers/' + id] = null
+    return updates
+}
+
 async function courseEnrolledDeletionDependencies(courseId) {
     let updates = {};
-
-    const enrolledStudents = (await fb.db.ref('courseStudents/' + courseId).once("value")).val()
+    const enrolledStudents = (await fb.db.ref(`courseStudents/${courseId}`).once("value")).val()
     if (enrolledStudents) {
-        updates['/courseStudents/' + courseId] = null
+        updates[`courseStudents/${courseId}`] = null
         Object.keys(enrolledStudents).forEach((studentId) => {
-            updates['/students/' + studentId + '/courses/' + courseId] = null
+            updates[`/students/${studentId}/courses/${courseId}`] = null
         })
     }
     return updates

8774f1de2d7f09c618592882aad7ce51d748787a
Eslam Fakhry <eslamPhakhry@gmail.com>
2019-09-05 13:22:06 +0200

    Add showError utility function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,14 +1,26 @@
 import {getTextWidth} from './textWidth'
 import store from "@/store";
 
-function showSnackbar(message,color){
-    store.commit('snackbars/SET_TOAST',{
+function showSnackbar(message, color) {
+    store.commit('snackbars/SET_TOAST', {
         message,
         color,
     })
 }
 
+function showError(code = 'GENERIC') {
+    switch (code) {
+        case "PERMISSION_DENIED":
+            showSnackbar('You have no authentication to complete this process', 'error')
+            break;
+        case "GENERIC":
+            showSnackbar('Something went wrong', 'error')
+            break;
+    }
+}
+
 export {
     getTextWidth,
-    showSnackbar
+    showSnackbar,
+    showError,
 }
